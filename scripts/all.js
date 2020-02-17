b59244914c48c7493d6e223562f6c62a50f63c22
Claudiu Bogdan Ciumedean <CBC@kfforsikring.dk>
2020-02-17 11:45:24 +0100

    maze

--- scripts/all.js
+++ scripts/all.js
@@ -1,43 +1,49 @@
-const insertSortAsc = arr => {
-    const n = arr.length;
+const grid = [
+    [ 1, 1, 1, 1, 1, 0, 0, 1, 1, 1 ],
+    [ 0, 1, 1, 1, 1, 1, 0, 1, 0, 1 ],
+    [ 0, 0, 1, 0, 1, 1, 1, 0, 0, 1 ],
+    [ 1, 0, 1, 1, 1, 0, 1, 1, 0, 1 ],
+    [ 0, 0, 0, 1, 0, 0, 0, 1, 0, 1 ],
+    [ 1, 0, 1, 1, 1, 0, 0, 1, 1, 0 ],
+    [ 0, 0, 0, 0, 1, 0, 0, 1, 0, 1 ],
+    [ 0, 1, 1, 1, 1, 1, 1, 1, 0, 0 ],
+    [ 1, 1, 1, 1, 1, 0, 0, 1, 1, 1 ],
+    [ 0, 0, 1, 0, 0, 1, 1, 0, 0, 1 ],
+];
+const gridSize = 10;
 
-    if(n === 0 || n === 1) {
-        return arr;
-    }
+class MazeGenerator {
+    generateMaze() {
+        const table = document.createElement('table');
 
-    for(let i = 1; i < arr.length; i++) {
-        const key = arr[i]; 
-        let j = i - 1;
+        for(let i = 0; i < gridSize; i++) {
+            const row = document.createElement('tr');
+            for(let j = 0; j < gridSize; j++) {
+                const cell = document.createElement('td');
+                const value = grid[i][j];
 
-        while(j >= 0 && arr[j] > key) {
-            arr[j + 1]  = arr[j];
-            j = j - 1;
+                value === 0 && cell.classList.add('blocked');
+                (i === 0 && j === 0) && cell.classList.add('start');
+                (i === gridSize - 1 && j === gridSize - 1) && cell.classList.add('end');
+                cell.dataset.idx = `${i},${j}`;
+                row.appendChild(cell);
+            }
+
+            table.appendChild(row);
         }
 
-        arr[j + 1] = key;
+        document.body.appendChild(table);
     }
-
-    return arr;
 }
 
-const insertSortDesc = arr => {
-    const n = arr.length;
 
-    if(n === 0 || n === 1) {
-        return arr;
+class Node {
+    constructor(x, y, dist) {
+        this.x = x;
+        this.y = y;
+        this.dist = dist;
     }
-
-    for(let i = 1; i < arr.length; i++) {
-        const key = arr[i];
-        let j = i - 1;
-
-        while(j >= 0 && arr[j] < key) {
-            arr[j + 1]  = arr[j];
-            j = j - 1;
-        }
-
-        arr[j + 1] = key;
-    }
-
-    return arr;
 }
+
+const maze = new MazeGenerator();
+maze.generateMaze();
